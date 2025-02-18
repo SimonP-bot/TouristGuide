5b82164475d9b9fb6853b498e9adeaafd876d309
Sofie Broman <sofierytter@hotmail.com>
2025-02-18 10:40:39 +0100

    GetMapping() for eksterne html-sider.

--- src/main/java/com/example/touristguide/controller/TouristController.java
+++ src/main/java/com/example/touristguide/controller/TouristController.java
@@ -56,4 +56,19 @@ public class TouristController {
         model.addAttribute("attraction", deletedAttraction);
         return "index.html";
     }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
 }
