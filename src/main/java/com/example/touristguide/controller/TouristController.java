4ee0bec26e6bb9ab9f4ad6b1da268607d74332db
Sofie Broman <sofierytter@hotmail.com>
2025-02-19 21:16:24 +0100

    <form> for ny turistattraktion. Formular kan udfyldes, men ikke gemmes

--- src/main/java/com/example/touristguide/controller/TouristController.java
+++ src/main/java/com/example/touristguide/controller/TouristController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.HTML;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -39,14 +40,27 @@ public class TouristController {
         return "tags";
     }
 
-    @GetMapping("/attractions/add")
-    public String addAttraction(Model model, @RequestBody TouristAttraction newAttraction) {
-        model.addAttribute("attraction", touristService.addAttraction(newAttraction));
-        return "redirect:/attractionList";
+    @GetMapping("/attractions/add") //Bruges til at vise html-side - ikke sende data
+    public String addAttraction(Model model) {
+        model.addAttribute("attraction", new TouristAttraction()); //Opretter tom attraktion
+        model.addAttribute("cities", touristService.getCities()); //Henter liste over byer
+        model.addAttribute("tags", Tags.values()); //Henter tags-værdier
+        return "newAttraction"; //Viser html <form>
     }
 
 
     //@PostMapping("/attractions/save")
+
+    @PostMapping("/attractions/save")
+    public String saveAttraction(@ModelAttribute TouristAttraction attraction, @RequestParam(required = false) List<Tags> tags) {
+        if (tags != null) {
+            attraction.setTags(tags);
+        } else {
+            attraction.setTags(new ArrayList<>()); //Ingen tags = tom liste (undgår NullPointerException)
+        }
+        touristService.addAttraction(attraction);
+        return "redirect:/attractionList"; // Omdirigerer til listen over attraktioner
+    }
 
     @GetMapping("/attractions/{name}/edit")
     public String editAttraction(Model model, @PathVariable String name) {
