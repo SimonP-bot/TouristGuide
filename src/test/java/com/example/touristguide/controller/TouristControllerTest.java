4d326379977460c18a834b56c7bc05031f092d51
Sofie Broman <sofierytter@hotmail.com>
2025-03-03 20:39:33 +0100

    tests af getAttractionByName() og getTags()

--- src/test/java/com/example/touristguide/controller/TouristControllerTest.java
+++ src/test/java/com/example/touristguide/controller/TouristControllerTest.java
@@ -1,38 +1,39 @@
 package com.example.touristguide.controller;
 import com.example.touristguide.model.Tags;
 import com.example.touristguide.model.TouristAttraction;
-import com.example.touristguide.service.TouristService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.FlashAttributeResultMatchers;
-
-import java.awt.*;
-import java.util.Arrays;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(TouristController.class)
+import com.example.touristguide.service.TouristService;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@WebMvcTest(TouristController.class) //WebMvcTest-annotation = mocker service-laget
 class TouristGuideApplicationTests {
 
 
 
     @Autowired
-    private MockMvc mockMvc; //Simulerer requests vha objekt til simuleringen
+    private MockMvc mockMvc; //Simulerer requests vha objekt
 
     @MockitoBean
-    private TouristService touristService; //Bean-objekt i stedet for objekt af @Service
+    private TouristService touristService; //Mock'er @Service, så den hardcodet arrayList ikke er nødvendig
 
     @BeforeEach
     void setUp() {
+        Mockito.reset(touristService); //Nulstiller mock mellem hver test
 
 
 
@@ -42,11 +43,41 @@ class TouristGuideApplicationTests {
     void tearDown() {
     }
 
-    @Test
-    void showTouristAttractions() throws Exception {
+    @Test //Viser alle attraktioner
+    void testShowTouristAttractions() throws Exception {
+        when(touristService.getAllAttractions())
+                .thenReturn(List.of(new TouristAttraction("Eremitageslottet", "Jagtslot", "Klampenborg"),
+                        new TouristAttraction("Fredensborg Slot", "Slot", "Fredensborg")));
+
         mockMvc.perform(get("/attractions"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("attractionList"));
+                .andExpect(view().name("attractionList"))
+                .andExpect(model().attributeExists("attractions"));
+    }
+
+    @Test //Finder attraktion ud fra navn
+    void testGetAttractionByName() throws Exception {
+        TouristAttraction touristAttraction = new TouristAttraction("Eremitageslottet", "Jagtslot", "Klampenborg");
+
+        when(touristService.getAttractionByName("Eremitageslottet")).thenReturn(touristAttraction);
+
+        mockMvc.perform(get("/attractions/Eremitageslottet"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index")) //Returnerer 'index' da dette ikke er løst i koden endnu
+                .andExpect(model().attributeExists("attraction"));
+    }
+
+    @Test //Viser tags for en attraktion
+    void testGetTags() throws Exception {
+        List<Tags> tags = Arrays.asList(Tags.CHILD_FRIENDLY, Tags.FOR_FREE);
+
+        when(touristService.getTags("Eremitageslottet")).thenReturn(tags);
+
+        mockMvc.perform(get("/attractions/Eremitageslottet/tags"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("tags"))
+                .andExpect(model().attributeExists("tags"));
+
     }
     /*
 
